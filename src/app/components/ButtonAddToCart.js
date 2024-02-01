82031c788ebd86dab57cbf44445b2a72b1844fe6
dani <maildedani9@gmail.com>
2024-02-01 10:53:03 +0100

    ButtonAddToCard refactored

--- src/app/components/ButtonAddToCart.js
+++ src/app/components/ButtonAddToCart.js
@@ -45,48 +45,54 @@ export default function ButtonAddToCart({ item }) {
   };
 
 
-
-  const handleSelection = (size) => {
-    setSizeSelectedOption(size);
-  };
-
-  const showToastMessage = (item) => {
-    toast.success("Este producto se ha añadido a la cesta", {
-      position: toast.POSITION.TOP_CENTER,
-      autoClose: 2000,
-    });
+  const showToastMessage = (type) => {
+    if ( type === "success") {
+      toast.success("Este producto se ha añadido al carrito", {
+        position: toast.POSITION.TOP_CENTER,
+        autoClose: 2000,
+      });
+    }
+    if (type === "error") {
+      toast.error("Debes seleccionar color y talla", {
+        position: toast.POSITION.TOP_CENTER,
+        autoClose: 2000,
+      });
+    }
   };
 
   const handleAddItemToCard = () => {
-    addItemToCart(item, sizeSelectedOption);
-    showToastMessage(item);
+    if (colorSelectedOption && sizeSelectedOption) {
+      addItemToCart(item, sizeSelectedOption);
+      showToastMessage("success");
+    } else {
+      showToastMessage("error");
+    }
   };
 
+  useEffect(() => {
+    itemWithDetails && setColorSelectedOption(itemWithDetails.colors[0].color)
+    itemWithDetails && setSizeSelectedOption(itemWithDetails.colors[0].sizes[0])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+  useEffect(() => {
+    const selectedColor = itemWithDetails?.colors?.find((item) => { return item.color?.id === colorSelectedOption?.id })
+    selectedColor && setSizeSelectedOption(selectedColor.sizes[0])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [colorSelectedOption])
 
 
-  /*  useEffect(() => {
-         setSizeSelectedOption(item.sizes[0])
-         // eslint-disable-next-line react-hooks/exhaustive-deps
-     }, []) */
-
-useEffect(()=> {
-   setColorSelectedOption(itemWithDetails.colors[0].color)
-},[])
-
-
-
+  console.log(itemWithDetails, colorSelectedOption)
 
   return (
     <div className="flex flex-1 flex-col w-full mt-8 text-lg font-medium ">
       Colores
       <div className="flex mt-2 mb-4">
         {itemWithDetails && itemWithDetails.colors?.map((e) => {
-
           return (
             <div
               key={e.id}
               onClick={() => setColorSelectedOption(e.color)}
-              className={`w-5 h-5 border-2 border-[#929292] rounded-full inline-block mr-2 ${colorSelectedOption && e.color?.id === colorSelectedOption?.id ? "border-black" : "border-[#929292]"
+              className={`w-5 h-5 border-2 border-[#929292] rounded-full inline-block mr-2 cursor-pointer ${colorSelectedOption && e.color?.id === colorSelectedOption?.id ? "border-black" : "border-[#929292]"
                 }`}
               style={{ background: e?.color?.color }}
             ></div>
@@ -95,30 +101,30 @@ useEffect(()=> {
       </div>
       Tallas
       {colorSelectedOption &&
-       itemWithDetails.colors?.map((e) => {
-        if (colorSelectedOption?.id === e.color?.id) {
-          return (
-            <div key={e.color?.id} className="flex w-full flex-row">
-              {e.sizes?.map((size, index) => (
-                <div key={index} className="border mr-3 p-2 mt-2 font-normal size">
-                  {size?.name}
-                </div>
-              ))}
-            </div>
-          );
-        } else {
-          return null;
-        }
-      })
+        itemWithDetails.colors?.map((e, index) => {
+          if (colorSelectedOption?.id === e.color?.id) {
+            return (
+              <div key={index} className="flex w-full flex-row">
+                {e.sizes?.map((size, index) => (
+                  <div key={index} onClick={() => setSizeSelectedOption(size)} className={`flex border w-8 h-8 mr-3  mt-2 font-normal cursor-pointer ${sizeSelectedOption && sizeSelectedOption.id === size.id ? "bg-primary" : ""} `}   >
+                    <span className="mx-auto my-auto text-base">{size?.name}</span>
+                  </div>
+                ))}
+              </div>
+            );
+          } else {
+            return null;
+          }
+        })
 
       }
 
       <input
         className="flex mt-10 flex-1 p-4 font-normal text-xl text-white uppercase cursor-pointer text-center mb-8 focus:outline-none"
-        defaultValue="Añadir al carrito"
         readOnly
         onClick={handleAddItemToCard}
         style={{ backgroundColor: "#dac895" }}
+      defaultValue="Añadir al carrito"
       />
       <div className="flex">
         <HiLockClosed size={20} color="#dac895" className="my-auto" />{" "}
