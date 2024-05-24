f615ba1af8946029c4c8fe2a524cc2fcd3eb317e
dani <maildedani9@gmail.com>
2024-05-24 07:28:25 +0200

    wip redsys

--- src/app/components/FinalizeButton.js
+++ src/app/components/FinalizeButton.js
@@ -30,12 +30,15 @@ export default async function FinalizeButton() {
 
 
     return (
+
         data &&
+        <>
         <form  name="form" action="https://sis-t.redsys.es:25443/sis/realizarPago" method="POST">
             <input type="hidden" name="Ds_SignatureVersion" value="HMAC_SHA256_V1" />
-            <input type="hidden" name="Ds_MerchantParameters" value={data?.jsonData}/>
-            <input type="hidden" name="Ds_Signature"  value={data?.signature}/>
+            <input type="hidden" name="Ds_MerchantParameters" value={data.jsonData}/>
+            <input type="hidden" name="Ds_Signature"  value={data.signature}/>
             <button className="flex w-3/12 p-4 text-xl text-white text-center mb-8  cursor-pointer bg-primary justify-center mx-auto" type="submit">Realizar Pago</button>
         </form>
+        </>
     );
 }
--- src/app/components/RedsysForm.js
+++ src/app/components/RedsysForm.js
@@ -6,8 +6,8 @@ export default function Redsysform () {
     return (
         <form  name="form" action="https://sis-t.redsys.es:25443/sis/realizarPago" method="POST">
             <input type="hidden" name="Ds_SignatureVersion" value="HMAC_SHA256_V1" />
-            <input type="hidden" name="Ds_MerchantParameters" value={redsysData?.jsonData}/>
-            <input type="hidden" name="Ds_Signature"  value={redsysData?.signature}/>
+            <input type="hidden" name="Ds_MerchantParameters" value={redsysData.jsonData}/>
+            <input type="hidden" name="Ds_Signature"  value={redsysData.signature}/>
             <button className="flex w-3/12 p-4 text-xl text-white text-center mb-8  cursor-pointer bg-primary justify-center mx-auto" type="submit">Realizar Pago</button>
         </form>
     )
