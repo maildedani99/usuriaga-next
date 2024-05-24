6f7db70d9e2d614486b7f50af63115984b98b556
dani <maildedani9@gmail.com>
2024-05-24 16:05:41 +0200

    wip2 2

--- src/app/production/page.js
+++ src/app/production/page.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Modal from "../components/Modal";
 
 
@@ -8,6 +9,14 @@ export default function Production () {
 
         <div className="flex text-5xl justify-center w-full tracking-wider capitalize font-light text-primary text-center min-h-[700px]">
         <div className="flex flex-col font-semibold space-y-10 my-auto">
+            <Image
+                src="/logogrisprueba.png"
+                width={400}
+                height={400}
+                alt="Logo usuriaga"
+                className=" mx-auto mb-10"
+
+            />
 
         <span>Esta página en proceso de producción</span>
         <span>Pronto estara disponible!!</span>
