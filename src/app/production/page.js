e923c464abe4cbb9ec39afe1bce5ea3a9dc9011d
dani <maildedani9@gmail.com>
2024-05-24 15:52:42 +0200

    wip

---
+++ src/app/production/page.js
@@ -0,0 +1,18 @@
+import Modal from "../components/Modal";
+
+
+export default function Production () {
+
+    return (
+        <Modal>
+
+        <div className="flex text-5xl justify-center w-full tracking-wider capitalize font-light text-primary text-center min-h-[700px]">
+        <div className="flex flex-col font-semibold space-y-10 my-auto">
+
+        <span>Esta página en proceso de producción</span>
+        <span>Pronto estara disponible!!</span>
+        </div>
+    </div>
+        </Modal>
+    )
+}
