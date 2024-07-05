1c42326b00477833ad33ba27bfd9ba17f79b663a
dani <maildedani9@gmail.com>
2024-07-05 04:16:58 +0200

    cache config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,54 @@ const nextConfig = {
   images: {
     domains: ['res.cloudinary.com'],
   },
+  async headers() {
+    return [
+      {
+        // Deshabilitar la caché para todas las rutas
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, no-cache, must-revalidate, proxy-revalidate',
+          },
+          {
+            key: 'Pragma',
+            value: 'no-cache',
+          },
+          {
+            key: 'Expires',
+            value: '0',
+          },
+          {
+            key: 'Surrogate-Control',
+            value: 'no-store',
+          },
+        ],
+      },
+      {
+        // Deshabilitar la caché para imágenes servidas por Next.js
+        source: '/_next/image',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, no-cache, must-revalidate, proxy-revalidate',
+          },
+          {
+            key: 'Pragma',
+            value: 'no-cache',
+          },
+          {
+            key: 'Expires',
+            value: '0',
+          },
+          {
+            key: 'Surrogate-Control',
+            value: 'no-store',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
