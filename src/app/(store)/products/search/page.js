927446779e76e5d67eb42dba33c4600cc678545b
dani <maildedani9@gmail.com>
2024-07-16 09:08:05 +0200

    swr search

--- src/app/(store)/products/search/page.js
+++ src/app/(store)/products/search/page.js
@@ -1,13 +1,19 @@
+"use client"
+import useSWR from "swr";
+import Error from "../../../components/Error";
 import ProductsView from "../../../components/ProductsView";
 import Searcher from "../../../components/Searcher";
 import { getProductsStock } from "../../../lib/data";
+import { fetcher } from "../../../utils/fetcher";
 
-export default async function Search () {
+export default  function Search () {
 
-    const products = await getProductsStock();
+    const { data, error } = useSWR(`${process.env.NEXT_PUBLIC_API_URL}products/allStock`, fetcher);
+
+    if (error) return <Error />;
     
 
     return (
-        <Searcher products={products} />
+        <Searcher products={data} />
     )
 }
