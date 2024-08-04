128866ece5e5bcaecdf439884626bf7bdf3fb484
dani <maildedani9@gmail.com>
2024-08-04 08:53:15 +0200

    wip

--- src/app/(pay)/paymentKo/page.js
+++ src/app/(pay)/paymentKo/page.js
@@ -1,51 +1,43 @@
 'use client';
 
-import { Suspense } from 'react';
-import { useSearchParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
+import { useSearchParams } from 'next/navigation';
 import OrderPending from '../../components/OrderPending';
 import Spinner from '../../components/Spinner';
-import { comfirmOrder } from '../../lib/data';
 import Error from '../../components/Error';
 
 export default function PaymentKo() {
-    const searchParams = useSearchParams();
     const [paymentData, setPaymentData] = useState(null);
-
-    const onConfirmOrder = async (orderId) => {
-        const res = comfirmOrder(orderId);
-    }
+    const searchParams = useSearchParams(); // Llama al hook incondicionalmente
 
     useEffect(() => {
-        const dsMerchantParameters = searchParams.get('Ds_MerchantParameters');
+        if (typeof window !== 'undefined') { // Verifica que estamos en el cliente
+            const dsMerchantParameters = searchParams.get('Ds_MerchantParameters');
 
-        if (dsMerchantParameters) {
-            try {
-                // Decodificar los parámetros de Base64
-                const decodedParams = atob(dsMerchantParameters);
-                
-                // Parsear los parámetros decodificados
-                const parsedParams = JSON.parse(decodedParams);
-                //onConfirmOrder(parsedParams.Ds_Order)
-
-                parsedParams && setPaymentData(parsedParams);
-            } catch (error) {
-                console.error("Error decodificando los parámetros:", error);
+            if (dsMerchantParameters) {
+                try {
+                    // Decodificar los parámetros de Base64
+                    const decodedParams = atob(dsMerchantParameters);
+                    
+                    // Parsear los parámetros decodificados
+                    const parsedParams = JSON.parse(decodedParams);
+                    setPaymentData(parsedParams);
+                } catch (error) {
+                    console.error("Error decodificando los parámetros:", error);
+                }
             }
         }
     }, [searchParams]);
 
     return (
-        <Suspense fallback={<Spinner />}>
-            <div className="flex w-full h-screen">
-                <div className="mt-36 flex flex-1">
-                    {paymentData ?
-                        <OrderPending ds_order={paymentData.Ds_Order} />
-                        :
-                        <Error />
-                    }
-                </div>
+        <div className="flex w-full h-screen">
+            <div className="mt-36 flex flex-1">
+                {paymentData ? (
+                    <OrderPending ds_order={paymentData.Ds_Order} />
+                ) : (
+                    <Error />
+                )}
             </div>
-        </Suspense>
+        </div>
     );
 }
--- src/app/(pay)/paymentOk/page.js
+++ src/app/(pay)/paymentOk/page.js
@@ -1,40 +1,43 @@
 'use client';
+
+import { useEffect, useState } from 'react';
 import { useSearchParams } from 'next/navigation';
-import { Suspense, useEffect, useState } from 'react';
 import OrderComfirmed from '../../components/OrderComfirmed';
 import Spinner from '../../components/Spinner';
-import { comfirmOrder } from '../../lib/data';
 import Error from '../../components/Error';
 
 export default function PaymentOk() {
-    const searchParams = useSearchParams();
     const [paymentData, setPaymentData] = useState(null);
+    const searchParams = useSearchParams(); // Llama al hook incondicionalmente
 
     useEffect(() => {
-        const dsMerchantParameters = searchParams.get('Ds_MerchantParameters');
-        
-        if (dsMerchantParameters) {
-            try {
-                const decodedParams = atob(dsMerchantParameters);
-                const parsedParams = JSON.parse(decodedParams);
-                setPaymentData(parsedParams);
-            } catch (error) {
-                console.error("Error decoding parameters:", error);
+        if (typeof window !== 'undefined') { // Verifica que estamos en el cliente
+            const dsMerchantParameters = searchParams.get('Ds_MerchantParameters');
+
+            if (dsMerchantParameters) {
+                try {
+                    // Decodificar los parámetros de Base64
+                    const decodedParams = atob(dsMerchantParameters);
+                    
+                    // Parsear los parámetros decodificados
+                    const parsedParams = JSON.parse(decodedParams);
+                    setPaymentData(parsedParams);
+                } catch (error) {
+                    console.error("Error decodificando los parámetros:", error);
+                }
             }
         }
     }, [searchParams]);
 
     return (
-        <Suspense fallback={<Spinner />}>
-            <div className="flex w-full h-screen">
-                <div className="mt-36 flex flex-1">
-                    {paymentData ? 
-                        <OrderComfirmed ds_order={paymentData.Ds_Order} /> : 
-                        <Error />
-                    }
-                </div>
+        <div className="flex w-full h-screen">
+            <div className="mt-36 flex flex-1">
+                {paymentData ? (
+                    <OrderComfirmed ds_order={paymentData.Ds_Order} />
+                ) : (
+                    <Error />
+                )}
             </div>
-        </Suspense>
+        </div>
     );
 }
-
