001e8ca676eb3a9d3c74de2e74bcc88eee7e6bec
dani <maildedani9@gmail.com>
2024-08-04 08:58:42 +0200

    wip

--- src/app/(pay)/paymentOk/page.js
+++ src/app/(pay)/paymentOk/page.js
@@ -1,30 +1,34 @@
-'use client';
-
-import { useEffect, useState } from 'react';
+"use client"
 import { useSearchParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
 import OrderComfirmed from '../../components/OrderComfirmed';
 import Spinner from '../../components/Spinner';
+import { comfirmOrder } from '../../lib/data';
 import Error from '../../components/Error';
 
-export default function PaymentOk() {
+export default function PayOk() {
+    const searchParams = useSearchParams();
     const [paymentData, setPaymentData] = useState(null);
-    const searchParams = useSearchParams(); // Llama al hook incondicionalmente
+
+    const onConfirmOrder = async (orderId) => {
+        const res = comfirmOrder(orderId)
+    }
 
     useEffect(() => {
-        if (typeof window !== 'undefined') { // Verifica que estamos en el cliente
-            const dsMerchantParameters = searchParams.get('Ds_MerchantParameters');
+        const dsMerchantParameters = searchParams.get('Ds_MerchantParameters');
 
-            if (dsMerchantParameters) {
-                try {
-                    // Decodificar los parámetros de Base64
-                    const decodedParams = atob(dsMerchantParameters);
-                    
-                    // Parsear los parámetros decodificados
-                    const parsedParams = JSON.parse(decodedParams);
-                    setPaymentData(parsedParams);
-                } catch (error) {
-                    console.error("Error decodificando los parámetros:", error);
-                }
+        if (dsMerchantParameters) {
+            try {
+                // Decodificar los parámetros de Base64
+                const decodedParams = atob(dsMerchantParameters);
+                
+                // Parsear los parámetros decodificados
+                const parsedParams = JSON.parse(decodedParams);
+                onConfirmOrder(parsedParams.Ds_Order)
+
+                parsedParams && setPaymentData(parsedParams);
+            } catch (error) {
+                console.error("Error decodificando los parámetros:", error);
             }
         }
     }, [searchParams]);
@@ -32,11 +36,11 @@ export default function PaymentOk() {
     return (
         <div className="flex w-full h-screen">
             <div className="mt-36 flex flex-1">
-                {paymentData ? (
+                {paymentData ?
                     <OrderComfirmed ds_order={paymentData.Ds_Order} />
-                ) : (
+                    :
                     <Error />
-                )}
+                }
             </div>
         </div>
     );
